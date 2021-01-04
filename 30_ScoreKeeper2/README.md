9fa6b40f2d350706baa827d455f4a3570e002607
Rachel Murray <dcbeergoddess@gmail.com>
2021-01-04 17:36:01 -0500

    finished ScoreKeeper, going to start a new section with it refactored

--- 30_ScoreKeeper2/README.md
+++ 30_ScoreKeeper2/README.md
@@ -5,5 +5,28 @@
 - Use a select to create game score options. We will need to make sure there is code that you can't change the winningScore in the middle of the game
   - use change event instead of click
 
+## Bulma
+- Another style library that you can use in place of bootstrap etc.
+* [BULMA DOCS](https://bulma.io/)
+- does not make you depend on jquery or popper.js which are both dependencies of Bootstrap
+* UNDER Documentation/Overview/Start - [Find CDN](https://bulma.io/documentation/overview/start/) 
+```html
+  <!-- USING THIS MIN CDN LINK FOR THIS PROJECT -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+```
+
+* Using Card under Components
+* Uses a Grid System - Columns - Basics and Sizes
+* halfway across screen = is-half
+* h1 has a typography class from bulma, under Helpers - ACTUALLY IT ENDED UP BEING UNDER ELEMENTS/TITLE
+* Elements - Buttons in Bulma - For Score Keeping, color and size
+* Under Forms in Bulma - Select (for the winningScoreSelect)
+
+* BULMA tells you to wrap everything in a section with a class of section for spacing
+
+* Use color classes under Helpers/Color
+* Bulma has built in styles for disabled buttons
+
+
 
 
--- 30_ScoreKeeper2/app.js
+++ 30_ScoreKeeper2/app.js
@@ -21,8 +21,11 @@ p1Button.addEventListener('click', function() {
     p1Score += 1;
     if (p1Score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add('winner');
-      p2Display.classList.add('loser');
+      p1Display.classList.add('has-text-success');
+      p2Display.classList.add('has-text-danger');
+      //BULMA DISABLED CLASSES
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
     p1Display.textContent = p1Score
   }
@@ -33,8 +36,10 @@ p2Button.addEventListener('click', function() {
     p2Score += 1;
     if (p2Score === winningScore) {
       isGameOver = true;
-      p2Display.classList.add('winner');
-      p1Display.classList.add('loser');
+      p2Display.classList.add('has-text-success');
+      p1Display.classList.add('has-text-danger');
+      p1Button.disabled = true;
+      p2Button.disabled = true;
     }
     p2Display.textContent = p2Score
   }
@@ -55,6 +60,9 @@ function reset() {
   p2Score = 0;
   p1Display.textContent = 0;
   p2Display.textContent = 0;
-  p1Display.classList.remove('winner', 'loser');
-  p2Display.classList.remove('winner', 'loser');
+  p1Display.classList.remove('has-text-success', 'has-text-danger');
+  p2Display.classList.remove('has-text-success', 'has-text-danger');
+  p1Button.disabled = false;
+  p2Button.disabled = false;
+
 }
