60241f0b3687d3f5aeb469489201c4e25afc8223
Rachel Murray <dcbeergoddess@gmail.com>
2021-03-15 18:06:08 -0400

    passing data to template

--- 38_NODE_EXPRESS_TEMPLATING/01_templating_demo/index.js
+++ 38_NODE_EXPRESS_TEMPLATING/01_templating_demo/index.js
@@ -12,6 +12,14 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+//GENERATE RANDOM NUMBER
+app.get('/rand', (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  //PASS IN SECOND ARGUEMENT OF KEY VALUE PAIRS
+  res.render('random', { rand: num });
+  //WHATEVER NUM IS WILL BE AVAILABLE IN TEMPLATE UNDER RAND
+})
+
 app.listen(PORT, () => {
   console.log(`LISTENING ON http://localhost:${PORT}`)
 })
