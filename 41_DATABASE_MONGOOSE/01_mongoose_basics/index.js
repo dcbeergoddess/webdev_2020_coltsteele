0b93d089fe954f2e10cc3fb6aeb1578414fc02ec
Rachel Murray <dcbeergoddess@gmail.com>
2021-03-23 16:54:28 -0400

    Insert Many into DB

--- 41_DATABASE_MONGOOSE/01_mongoose_basics/index.js
+++ 41_DATABASE_MONGOOSE/01_mongoose_basics/index.js
@@ -26,5 +26,19 @@ const movieSchema = new mongoose.Schema({
 // Tell Mongoose to make a model with that Schema --> Pass in String with name of Model (Singular and Capitalized, mongo creates collections "movies") --> then Schema name --> save to variable and this gives us a CLASS with the name of Movie
 const Movie = mongoose.model('Movie', movieSchema)
 //Make new instances of Movie class and save them to MongoDB
-const amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'}) 
-//NOTHING has been created in Mongo yet until you save it
+// const amadeus = new Movie({title: 'Amadeus', year: 1986, score: 9.2, rating: 'R'}) 
+//NOTHING has been created in Mongo yet until you save it - must call .save() for one instance of a model
+
+//INSERT MANY --> Not Used very often 
+// TAKES TIME - RETURNS A PROMISE - DO NOT NEED TO CALL SAVE for this Process
+Movie.insertMany([
+  { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+  { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+  { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+  { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+  { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+]) //chain on a .then for the data (probably and err handling too)
+  .then(data => {
+    console.log("IT WORKED!");
+    console.log(data);
+  })
--- 41_DATABASE_MONGOOSE/README.md
+++ 41_DATABASE_MONGOOSE/README.md
@@ -83,7 +83,22 @@ mongoose.connect('mongodb://localhost:27017/movieApp', {useNewUrlParser: true, u
 
 ### Insert Many
 
+```js
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+])
+  .then(data => {
+    console.log("IT WORKED!");
+    console.log(data);
+  })
+```
+
 ### Finding With Mongoose
+
 
 ### Updating With Mongoose
 
