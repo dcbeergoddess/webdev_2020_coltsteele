1aa58362ef2f36d2a65312e88359c63315c0fa03
Rachel Murray <dcbeergoddess@gmail.com>
2021-03-30 14:18:45 -0400

    index.js template to initialize

---
+++ 42_DATABASE_EXPRESS_MONGOOSE/02_templates/index_initial.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+PORT = 8080
+const path = require('path')
+//WILL MOVE LATER - MONGOOSE
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/shopApp', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {
+  console.log("MONGO CONNECTION OPEN")
+})
+.catch(err => {
+  console.log("OH NO MONGO CONNECTION ERROR!")
+  console.log(err)
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.send('THIS IS THE HOME PAGE!!!')
+});
+
+app.get('*', (req, res) => {
+  res.send(`I DO NOT KNOW THAT PATH!!!!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`LISTENING ON http://localhost:${PORT}` )
+}); 
