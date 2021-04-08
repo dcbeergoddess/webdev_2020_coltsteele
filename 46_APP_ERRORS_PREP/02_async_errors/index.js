982b389807ba36b7ebbaa96bde23761ae377a8ce
Rachel Murray <dcbeergoddess@gmail.com>
2021-04-08 14:56:42 -0400

    DRY Async Error Handeling

--- 46_APP_ERRORS_PREP/02_async_errors/index.js
+++ 46_APP_ERRORS_PREP/02_async_errors/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-PORT = 3030;
+const PORT = 3030;
 const path = require('path');
 //WILL MOVE LATER - MONGOOSE
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
-const AppError = require('./AppError')
+const AppError = require('./AppError');
 
 //REQUIRE MODELS
 const Product = require('./models/product');
@@ -35,7 +35,14 @@ app.use(methodOverride('_method'));
 ///////////////////ARRAY CATEGORIES//////////////
 //******************************************** */
 const categories = ['fruit', 'vegetable', 'dairy', 'fungi'];
-
+//******************************************** */
+////////////////ERROR HANDLER////////////////////
+//******************************************** */
+function wrapAsync(fn){
+  return function(req, res, next){
+    fn(req, res, next).catch(e => next(e));
+  }
+};
 //******************************************** */
 ///////////////////ROUTES////////////////////////
 //******************************************** */
@@ -44,8 +51,7 @@ app.get('/', (req, res) => {
   res.send('THIS IS THE HOME PAGE!!!');
 });
 //INDEX PAGE - GET ALL PRODUCTS - async (takes time)
-app.get('/products', async (req, res) => {
-  try {
+app.get('/products', wrapAsync(async (req, res) => {
     const { category } = req.query;
     if(category){
       // const products = await Product.find({category: category})
@@ -56,10 +62,7 @@ app.get('/products', async (req, res) => {
         // console.log(products);
       res.render('products/index', { products, category: 'All' });
     }
-  } catch(e) {
-    next(e);
-  }
-});
+}));
 
 // 1. SERVE UP FORM TO CREATE NEW PRODUCTS - not doing anything async
 app.get('/products/new', (req, res) => {
@@ -67,7 +70,7 @@ app.get('/products/new', (req, res) => {
   res.render('products/new', { categories });
 });
 // 2. ROUTE TO POST FORM WHEN SUBMIT
-app.post('/products', async (req, res, next) => {
+app.post('/products', wrapAsync (async (req, res, next) => {
   try {
     const newProduct = new Product(req.body);
     await newProduct.save();
@@ -76,46 +79,33 @@ app.post('/products', async (req, res, next) => {
   } catch(e){
     next(e);
   } 
-});
+}));
 // SINGLE PRODUCT PAGE
-app.get('/products/:id', async (req, res, next) => {
-  try {
+app.get('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     if(!product){
       throw new AppError('Product Not Found', 404)
     };
     res.render('products/show', { product });
-  } catch(e) {
-    next(e);
-  }
-});
+}));
 //UPDATE PRODUCT
-app.get('/products/:id/edit', async (req, res, next) => {
-  try {
+app.get('/products/:id/edit', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findById(id);
     if(!product){
       throw new AppError('Product Not Found', 404)
     };
     res.render('products/edit', { product, categories });
-  } catch(e) {
-    next(e);
-  }
-
-});
+}));
 //PUT REQUEST TO REPLACE ENTIRE OBJECT WITH UPDATED INFO
-app.put('/products/:id', async (req, res, next) => {
-  try {
+app.put('/products/:id', wrapAsync(async (req, res, next) => {
     const { id } = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
     res.redirect(`/products/${product._id}`); 
-  } catch(e) {
-    next(e);
-  }
-});
+}));
 //DELETE PRODUCT
-app.delete('/products/:id', async (req, res, next) => {
+app.delete('/products/:id', wrapAsync(async (req, res, next) => {
   try {
     const { id } = req.params;
     const deletedProduct = await Product.findByIdAndDelete(id);
@@ -123,11 +113,12 @@ app.delete('/products/:id', async (req, res, next) => {
   } catch(e) {
     next(e);
   }
-});
+}));
 
 //******************************************** */
 ////////DEFAULT ERROR/PORT LISTENING/////////////
 //******************************************** */
+
 app.use((err, req, res, next) => {
   const { status = 500, message = 'Something Went Wrong'} = err;
   res.status(status).send(message);
